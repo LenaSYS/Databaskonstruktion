60a731a1940da6b4c7e669c6cff8aaa654746d72
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2018-10-09 18:21:54 +0200

    Example 7 update

--- DotNet/dbsk7_2018/Controllers/HomeController.cs
+++ DotNet/dbsk7_2018/Controllers/HomeController.cs
@@ -4,15 +4,22 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using dbsk7_2018.Models;
+using dbsk6_2018.Models;
 
-namespace dbsk7_2018.Controllers
+namespace dbsk6_2018.Controllers
 {
     public class HomeController : Controller
     {
+        private CustomersModel sp = new CustomersModel();
+
         public IActionResult Index()
         {
-            ViewBag.SomeText = "Some text passed from the controller using the ViewBag";
+            return View();
+        }
+
+        public IActionResult SearchCustomers(string name)
+        {
+            ViewBag.SearchResults = sp.SearchCustomers(name);
             return View();
         }
     }
---
+++ DotNet/dbsk7_2018/Models/CustomersModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace dbsk6_2018.Models
+{
+    public class CustomersModel
+    {
+        private string connectionString = "Server=localhost;Database=a00leifo;User ID=myusername;Password=mypassword;Pooling=false;SslMode=none;";
+
+        public CustomersModel()
+        {
+
+        }
+
+        public DataTable SearchCustomers(string name)
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM CUSTOMER WHERE name LIKE @NAME;", dbcon);
+            adapter.SelectCommand.Parameters.AddWithValue("@NAME", "%" + name + "%");
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable CustomersTable = ds.Tables["result"];
+            dbcon.Close();
+            return CustomersTable;
+        }
+    }
+}
+
+
+/*
+ *
+ *
+
+       public DataTable SearchStudents(string name, string studyProgram)
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Student WHERE name LIKE @NAME AND studyProgram=@SPROGRAM;", dbcon);
+            adapter.SelectCommand.Parameters.AddWithValue("@NAME", "%" + name + "%");
+            adapter.SelectCommand.Parameters.AddWithValue("@SPROGRAM", studyProgram);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable StudentsTable = ds.Tables["result"];
+            dbcon.Close();
+            return StudentsTable;
+        }
+    }
+
+ * 
+ */
