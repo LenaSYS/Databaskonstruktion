9ccdc06c481b5c238f9e7839a8c2393849267e82
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2026-08-26 11:37:17 +0200

    Initial startup of PHP code

---
+++ PHP/PHP Example 1 Forms/PHPForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>Testing Form</h3>
+ 
+<?php
+    
+    // Function to debug parameters
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+?>
+ 
+<form action="PHPForm.php" method="POST">
+  Field1: <input type="text" name="field1" /><br>
+  Field2: <input type="text" name="field2" /><br>
+  Checkbox1: <input type="checkbox" name="checkbox0" /><br>
+  <input type="submit" />
+</form> 
+ 
+</body>
+</html>
